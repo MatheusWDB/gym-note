7133da65d14b322d6f230d3c6c341edf0a1035d3
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-11-30 14:20:07 -0300

    feat: Projeto criado

---
+++ backend/src/test/java/br/com/hematsu/gym_note/GymNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hematsu.gym_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
